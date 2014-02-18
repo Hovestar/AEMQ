bd2986120c0a05cad736577690f4f53d873f10a5
Conrad Hougen <conrad.hougen@gmail.com>
2014-02-18 16:48:41 -0700

    Removed unnecessary demo files

---
+++ CORTEX_M4_ATSAM4S_Atmel_Studio/Debug/src/Common-Demo-Source/countsem.d
@@ -0,0 +1,59 @@
+src/Common-Demo-Source/countsem.d src/Common-Demo-Source/countsem.o: \
+ ../src/Common-Demo-Source/countsem.c \
+ ../src/asf/thirdparty/FreeRTOS/include/FreeRTOS.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/include/stddef.h \
+ ../src/asf/thirdparty/FreeRTOS/include/projdefs.h \
+ ../src/FreeRTOSConfig.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/include/stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/_ansi.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/newlib.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/config.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/features.h \
+ ../src/asf/thirdparty/FreeRTOS/include/portable.h \
+ ../src/asf/thirdparty/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ ../src/asf/thirdparty/FreeRTOS/include/mpu_wrappers.h \
+ ../src/asf/thirdparty/FreeRTOS/include/task.h \
+ ../src/asf/thirdparty/FreeRTOS/include/list.h \
+ ../src/asf/thirdparty/FreeRTOS/include/semphr.h \
+ ../src/asf/thirdparty/FreeRTOS/include/queue.h \
+ ../src/Common-Demo-Source/include/countsem.h
+
+../src/asf/thirdparty/FreeRTOS/include/FreeRTOS.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/include/stddef.h:
+
+../src/asf/thirdparty/FreeRTOS/include/projdefs.h:
+
+../src/FreeRTOSConfig.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/include/stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/_ansi.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/newlib.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/config.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\arm\ gcc\native\4.7.3.1029\arm-gnu-toolchain\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/features.h:
+
+../src/asf/thirdparty/FreeRTOS/include/portable.h:
+
+../src/asf/thirdparty/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+
+../src/asf/thirdparty/FreeRTOS/include/mpu_wrappers.h:
+
+../src/asf/thirdparty/FreeRTOS/include/task.h:
+
+../src/asf/thirdparty/FreeRTOS/include/list.h:
+
+../src/asf/thirdparty/FreeRTOS/include/semphr.h:
+
+../src/asf/thirdparty/FreeRTOS/include/queue.h:
+
+../src/Common-Demo-Source/include/countsem.h:
